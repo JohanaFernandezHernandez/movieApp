cf2bd59ba3ed5c1e831dc35bcb19bb0f1f45f45d
JohanaFernandezHernandez <najofe5@gmail.com>
2024-03-12 15:59:04 +0100

    fix: https

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ const apiKey = import.meta.env.VITE_API_KEY;
 
 export async function fetchData(searchMovie) {
 
-  const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchMovie}`;
+  const url = `https://www.omdbapi.com/?apikey=${apiKey}&s=${searchMovie}`;
   if (searchMovie === "") return null;
 
   try {
